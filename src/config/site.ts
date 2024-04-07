f487607541df14d55769b45fd9eb095758c95a18
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-04-08 00:20:23 +0500

    init

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "mbsatimov/ui",
+    url: "https://ui-mbsatimov.vercel.app",
+    ogImage: "https://ui-mbsatimov.vercel.app/avatar.png",
+    description:
+      "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    links: {
+      github: "https://github.com/mbsatimov/ui",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
